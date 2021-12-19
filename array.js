81c40e104a881081bc14773d23380d4ccd6ca3bb
Mayusha04 <mayushachowdary95@gmail.com>
2021-12-19 22:51:55 +0530

    hi

---
+++ array.js
@@ -0,0 +1,26 @@
+var array =["red","blue","orange","green",1,2,4,7];
+
+console.log(array[3]);
+
+ array.push("yellow"); //push
+console.log(array);
+
+ array.unshift("good");//unshift
+ console.log(array);
+
+array[2]="Hiii";  //inserting new element
+console.log(array);
+
+array.pop();//pop
+ console.log(array);
+
+console.log(array.length);//length
+
+array.shift();  //shift
+ console.log(array);
+
+array.sort();    //sort
+console.log(array);
+
+array.reverse();    //reverse
+console.log(array);
